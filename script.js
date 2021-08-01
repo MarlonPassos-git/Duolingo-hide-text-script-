4057b3fdd4aadf9099fbc9853bf8a8b1a494be4f
Marlon Passos <63638196+MarlonPassos-git@users.noreply.github.com>
2021-07-31 21:01:19 -0300

    Create a script Js

---
+++ script.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name         Duolingo Hide Text
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        *://*.duolingo.com/*
+// @icon         https://www.google.com/s2/favicons?domain=duolingo.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    let loop = setInterval(()=> {
+    let phase = document.querySelector('._1KUxv [data-test="hint-sentence"]')
+
+    phase.classList.add('hideText')
+
+    hasStyle()
+
+}, 1000)
+    loop()
+
+    function hasStyle() {
+
+        let b = document.querySelector('.stylePlugin')
+        if (b == null) {
+            document.body.insertAdjacentHTML('beforeend', '<style class="stylePlugin"> .hideText {color: #FFF;}.hideText:hover {color: #3c3c3c}</style>')
+        }
+    }
+
+})();
