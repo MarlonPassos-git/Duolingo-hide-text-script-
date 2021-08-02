d1d866b097f51e4ea71c2a09ca5e7f9ef2203bf7
Marlon Passos <63638196+MarlonPassos-git@users.noreply.github.com>
2021-08-01 21:08:14 -0300

    faster and new function
    automatically show the sentence after we send the reply, set the time for the function to be activated to 500ms so we don't see anything at the beginning

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 // ==UserScript==
 // @name         Duolingo Hide Text
 // @namespace    http://tampermonkey.net/
-// @version      0.1.1
-// @description  try to take over the world!
+// @version      0.1.3
 // @author       You
 // @match        *://*.duolingo.com/*
 // @icon         https://www.google.com/s2/favicons?domain=duolingo.com
@@ -15,14 +14,19 @@
     let loop = setInterval(()=> {
     let phase = document.querySelector('._1KUxv [data-test="hint-sentence"]')
     let sond = document.querySelector('._21LCG')
+    let answer = document.querySelector('.YQ0lZ ')
 
     if (sond) {
         phase.classList.add('hideText')
     }
 
+    if (answer) {
+        phase.classList.remove('hideText')
+    }
+
     hasStyle()
 
-}, 1000)
+}, 500)
     loop()
 
     function hasStyle() {
